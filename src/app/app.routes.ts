02b2c7b19667fe152c8ef926bb6f44b079f6c5a0
T Prayag <91512981+Prayag-09@users.noreply.github.com>
2024-07-10 07:22:32 +0000

    approute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'budget-planner',loadChildren:()=> import('./budget-planner/budget-planner.module').then(m=> m.BudgetPlannerModule)}
+  { path: 'budget-planner', loadChildren: () => import('./budget-planner/budget-planner.module').then(m => m.BudgetPlannerModule) },
+  { path: '', redirectTo: 'budget-planner/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'budget-planner/login' }
 ];
-
